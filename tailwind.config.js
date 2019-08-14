eec615c92f073aad5d8ac28a9435fc3f67cf6ba6
Colin Lees <colin.lees@stockport.gov.uk>
2019-08-14 08:57:55 +0100

    chore(styles): 90% complete for dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,18 @@ module.exports = {
     fontFamily: {
       'title': ['Source Sans Pro', 'sans-serif'],
       'body': ['Noto Sans', 'Arial', 'sans-serif']
+    },
+    screens: {
+      'xs': {'max': '639px'},
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
     }
   },
   variants: {},
   plugins: [
-    require('tailwindcss-multi-column')()
+    require('tailwindcss-multi-column')(),
+    require('postcss-nesting')
   ],
 }

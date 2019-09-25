fb7f2f3f3f97c529b98f3dada0a6a3dc1311903c
Colin Lees <colinleesa7x@gmail.com>
2019-09-25 22:08:45 +0100

    major(Form/Nunjucks): add example form page and render pages using Nunjucks for shared page elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'xs': {'max': '639px'}
+        'xs': { 'max': '639px' }
       },
       backgroundColor: {
         'd-10': 'rgba(0, 0, 0, 0.10)',
@@ -15,11 +15,48 @@ module.exports = {
     fontFamily: {
       'title': ['Source Sans Pro', 'sans-serif'],
       'body': ['Noto Sans', 'Arial', 'sans-serif']
-    }
+    },
+    customForms: theme => ({
+      default: {
+        checkbox: {
+          width: theme('spacing.8'),
+          height: theme('spacing.8'),
+          borderColor: theme('colors.gray[900]'),
+          borderWidth: theme('borderWidth.2'),
+          borderRadius: theme('borderRadius.none'),
+          iconColor: theme('colors.gray[900]'),
+          '&:focus' : {
+            boxShadow: `0 0 0 2px ${theme('colors.orange[500]')}`,
+            borderColor: theme('colors.gray[900]')
+          },
+          '&:checked': {
+            borderColor: theme('colors.gray[900]'),
+            backgroundColor: theme('colors.white'),
+          },
+        },
+        radio: {
+          width: theme('spacing.8'),
+          height: theme('spacing.8'),
+          borderColor: theme('colors.gray[900]'),
+          borderWidth: theme('borderWidth.2'),
+          iconColor: theme('colors.gray[900]'),
+          icon: iconColor => `<svg viewBox="0 0 16 16" fill="${iconColor}" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="4"/></svg>`,
+          '&:focus' : {
+            boxShadow: `0 0 0 2px ${theme('colors.orange[500]')}`,
+            borderColor: theme('colors.gray[900]')
+          },
+          '&:checked': {
+            borderColor: theme('colors.gray[900]'),
+            backgroundColor: theme('colors.white'),
+          },
+        }
+      }
+    })
   },
   variants: {},
   plugins: [
     require('tailwindcss-multi-column')(),
-    require('postcss-nesting')
+    require('postcss-nesting'),
+    require('@tailwindcss/custom-forms')
   ],
 }
